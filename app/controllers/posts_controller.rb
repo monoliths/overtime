695bcdf21c4af59aae86bcb59d4ab5ac3cdb12d9
Gerald Hernandez <gerald2557@gmail.com>
2017-01-27 01:33:22 +0000

    added basic test for post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,26 @@
 class PostsController < ApplicationController
+  def show
+    @post = Post.find_by_id(params[:id])
+  end
   def index
-    
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render @post
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:date, :rationale)
   end
 end
