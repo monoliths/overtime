19dada131e319e7738d0eb6a5707032486ac53ad
gerald hernandez <gerald2557@gmail.com>
2017-02-15 23:35:17 -0800

    refactored posts page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
+@luneth = User.create!(email: "luneth@example.com", password: "asdasd", password_confirmation: "asdasd", first_name: "luneth", last_name: "l")
+@ramiel = User.create(email: "ramiel@example.com", password: "asdasd", password_confirmation: "asdasd", first_name: "ramiel", last_name: "r")
+@artemios = User.create(email: "artemios@example.com", password: "asdasd", password_confirmation: "asdasd", first_name: "artemios", last_name: "art")
+users = [@luneth, @ramiel, @artemios]
+
+puts '3 users created'
+
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rat content")
+  Post.create(date: Date.today, rationale: "#{post} rat content", user_id: users[post % 3].id)
 end
 
 puts "100 posts have been created"
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,25 +1,37 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    @user = User.create(email: "test@test.com", password: "asdasd", password_confirmation: "asdasd", first_name: "bob", last_name: "smith")
+    login_as(@user, :scope => :user)
+  end
+
   describe 'index' do
-    it 'can be reached successfully' do
+    before do
       visit posts_path
+    end
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of post' do
+      expect(page).to have_content("Posts Index")
+    end
+
+    it 'has a list of posts' do
+      @post1 = @user.posts.create(date: Date.today, rationale: 'post 1')
+      @post2 = @user.posts.create(date: Date.today, rationale: 'post 2')
+      @post3 = @user.posts.create(date: Date.today, rationale: 'post 3')
+      # need to revisit the post index since we created post records
       visit posts_path
-      expect(page).to have_content(/Posts/)
+      expect(page).to have_content(/post 1|post 2|post3/)
     end
   end
 
   describe 'creation' do
     before do
-      @user = User.create(email: "test@test.com", password: "asdasd", password_confirmation: "asdasd", first_name: "bob", last_name: "smith")
-      login_as(@user, :scope => :user)
       visit new_post_path
     end
-
     it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
