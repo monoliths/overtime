61872965d11d31ccc28bdd4d248552369d397e32
gerald hernandez <gerald2557@gmail.com>
2017-02-28 22:04:31 -0800

    Added status to admin panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-@luneth = User.create!(email: "luneth@example.com", password: "asdasd", password_confirmation: "asdasd", first_name: "luneth", last_name: "l")
-@ramiel = User.create(email: "ramiel@example.com", password: "asdasd", password_confirmation: "asdasd", first_name: "ramiel", last_name: "r")
-@artemios = User.create(email: "artemios@example.com", password: "asdasd", password_confirmation: "asdasd", first_name: "artemios", last_name: "art")
-users = [@luneth, @ramiel, @artemios]
+luneth = User.create!(email: "luneth@example.com", password: "asdasd", password_confirmation: "asdasd", first_name: "luneth", last_name: "l")
+ramiel = User.create(email: "ramiel@example.com", password: "asdasd", password_confirmation: "asdasd", first_name: "ramiel", last_name: "r")
+artemios = User.create(email: "artemios@example.com", password: "asdasd", password_confirmation: "asdasd", first_name: "artemios", last_name: "art")
+users = [luneth, ramiel, artemios]
 
 puts '3 users created'
 
@@ -10,3 +10,5 @@ puts '3 users created'
 end
 
 puts "100 posts have been created"
+
+AdminUser.create(email: "gerald@example.com", password: "123123", password_confirmation: "123123", first_name: "artemios", last_name: "art")
