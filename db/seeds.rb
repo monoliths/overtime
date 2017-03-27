680ec6efa4982658a5e68cd149deccb93b54518d
gerald hernandez <gerald2557@gmail.com>
2017-03-26 19:28:11 -0700

    updated the audit_log factory and 6 day defaults

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ artemios = User.create!(email: "artemios@example.com",
 
 users = [luneth, ramiel, artemios]
 
-puts '3 users created'
+puts "3 users created"
 
 100.times do |post|
   Post.create(date: Date.today, rationale: "#{post} rat content", user_id: users[post % 3].id, overtime_request: 2.5)
@@ -29,9 +29,18 @@ end
 
 puts "100 posts have been created"
 
+100.times do |audit_log|
+  AuditLog.create(user_id: users.first.id, status: 0, start_date: (Date.today - 1.day))
+  users.shuffle!
+end
+
+puts "100 audit logs created"
+
 AdminUser.create(email: "gerald@example.com",
                   password: "123123",
                   password_confirmation: "123123",
                   first_name: "gerald",
                   last_name: "hernandez",
                   phone: "9499231222")
+
+puts "1 admin user created"
--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :audit_log do
-    user nil
-    status 1
-    start_date "2017-03-25"
-    end_date "2017-03-25"
+    user 
+    status 0
+    start_date (Date.today - 1.day)
+    end_date nil
   end
 end
